da2b8df32a5853beff2abfb5bf4d56ea59fd94f9
Uğur KARAASLAN <62725213+uk3435@users.noreply.github.com>
2021-06-04 02:17:09 +0300

    Create sentiment_.py

---
+++ sentiment_.py
@@ -0,0 +1,10 @@
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
+
+model = AutoModelForSequenceClassification.from_pretrained("savasy/bert-base-turkish-sentiment-cased")
+tokenizer = AutoTokenizer.from_pretrained("savasy/bert-base-turkish-sentiment-cased")
+sa= pipeline("sentiment-analysis", tokenizer=tokenizer, model=model)
+
+def analizEt(text):
+
+    p = sa(text)
+    print(p)
